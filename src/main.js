ae3306483d66d5d4e5daa22d645c5557a5089d48
lee <white11190709@gmail.com>
2020-10-26 17:42:26 +0800

    在專案中加入 Bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Bcard msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Bcard from '@/components/Bcard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Bcard
   }
 }
 </script>
